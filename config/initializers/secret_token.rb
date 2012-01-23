8f5ff32996404ee7fab1504c19185247bd9dea2e
selko24 <denis.selic24@gmail.com>
2012-01-23 11:41:50 +0100

    Zacetni upload

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Digg::Application.config.secret_token = '73f96e5cbea5149ae9f8ded5827ce01cc9d91d2275168c4369a11b6e1c537c21b701b3e5010a573bab67f2e88bea1c5dab206e922551e0753eb8cfb95c806636'
